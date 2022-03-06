b26250eb23799db3efbcbafe2860f87f76992176
laphisboy <laphisboy@gmail.com>
2022-03-06 15:51:39 +0900

    now can convert rendered images to mp4

---
+++ png2mp4.py
@@ -0,0 +1,48 @@
+import imageio
+import os
+import glob
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--scene', type=str,
+                        choices=['ship','mic','chair','lego','drums','ficus','materials','hotdog'])
+
+    parser.add_argument('--view_type', type=str,
+                        choices=['nearest','dense','sparse','random','far','fixed-nearest','fixed-sparse'])                    
+
+    parser.add_argument('--ckpt', type=str)
+
+    parser.add_argument('--num_src_views', type=int)
+
+    parser.add_argument('--source', type=str, default='train')
+
+    parser.add_argument('--target', type=str, default='test')
+
+    args = parser.parse_args()
+
+    ckpt = args.ckpt
+    num_src_views = args.num_src_views
+    source = args.source
+    target = args.target
+    view_type = args.view_type
+
+    scene = args.scene
+
+    base_path = f'/mnt/hdd/youngsun/mvsnerf_timing/results/{ckpt[:-4]}/blender-{num_src_views}-{view_type}-{source}-{target}'
+    scene_path = os.path.join(base_path, scene) + '*.png'
+
+    file_list = glob.glob(scene_path)
+    file_list.sort()
+
+    save_path = f'/mnt/hdd/youngsun/mvsnerf_timing/results_video/{ckpt[:-4]}/blender-{num_src_views}-{view_type}-{source}-{target}/'
+    os.makedirs(save_path, exist_ok=True)
+
+    save_video_path = os.path.join(save_path, scene) + '.mp4'
+
+    writer = imageio.get_writer(save_video_path, fps=15)
+
+    for im in file_list:
+        writer.append_data(imageio.imread(im))
+    writer.close()
